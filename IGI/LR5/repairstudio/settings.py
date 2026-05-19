b4d99b05fd961ccb5f04a6c94f14653d4f9495aa
AlexITPS <kalmng563@gmail.com>
2026-05-19 03:35:33 +0300

    Update ALLOWED_HOSTS for hosting

--- IGI/LR5/repairstudio/settings.py
+++ IGI/LR5/repairstudio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g^0&z7aop(8g2ew*8dr+ofuzc882roo421b@@radlv%0iyc-3_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['AlexITPS.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
